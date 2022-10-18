38b712ac2f26cc7c5a77bbd193a0552c59369f24
이민석 <dat0802@gmail.com>
2022-10-18 23:33:29 +0900

    etc: swagger 설치 및 경로 설정

--- shop/settings.py
+++ shop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "drf_yasg",
 ]
 
 LOCAL_APPS = ["products"]
--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,34 @@
-from django.urls import path, include
+from django.urls import include, path, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Shop_Service API",
+        default_version="v0.1",
+        description="Shop_Service API 문서",
+        terms_of_service="https://github.com/leeminseok8/shop_service_test",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("shop/products/", include("products.urls")),
+    # swagger
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^doc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
